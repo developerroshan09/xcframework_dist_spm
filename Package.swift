ac5b20e96bf87314f3a52482d7b3ce224fda3390
developerroshan09 <149877862+developerroshan09@users.noreply.github.com>
2026-01-06 21:18:21 +0545

    Release 1.0.9

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ComposeApp",
-            url: "https://github.com/developerroshan09/xcframework_dis_spm/releases/download/1.0.0/ComposeApp.xcframework.zip",
+            url: "https://github.com/developerroshan09/xcframework_dist_spm/releases/download/1.0.0/ComposeApp.xcframework.zip",
             checksum: "2e87014f0c3ac67806c76aeff622e74dfb2329756a1bef5debe59d2a813124ef"
         )
     ]
